f42d835f36d73f9fede3df3d3160520b9c111d23
Vero519 <voviedo6@gmail.com>
2024-07-19 17:23:20 +0000

    Pending changes exported from your codespace

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_guardo = jsonify(todos)
+    return json_guardo
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
